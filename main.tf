6df8e45f6817c960c7712672729a7051aba0d313
eman <emanadel.104@gmail.com>
2022-07-31 23:23:20 +0100

    setting aws region

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+    required_version = ">= 0.12"
+
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = ">= 3.24"
+        }
+    }
+}
+
+variable "aws_region"{
+    type = string
+    default = "us-east-1"
+}
+
+provider "aws"{
+
+    region = var.aws_region
+}
